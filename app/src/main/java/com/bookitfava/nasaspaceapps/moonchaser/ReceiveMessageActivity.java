7b5fcf8c9bbc2ef3035c0c4560ee92d7be5c76e6
Ferid <ferid.dragon@hotmail.com>
2016-04-22 18:49:38 +0200

    ReceiveMessageActivity

--- app/src/main/java/com/bookitfava/nasaspaceapps/moonchaser/ReceiveMessageActivity.java
+++ app/src/main/java/com/bookitfava/nasaspaceapps/moonchaser/ReceiveMessageActivity.java
@@ -1,7 +1,10 @@
 package com.bookitfava.nasaspaceapps.moonchaser;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class ReceiveMessageActivity extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class ReceiveMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_messages);
+
+        Bundle bundle = getIntent().getExtras();
+        String messageText = bundle.getString("message");
+
+        TextView message = (TextView)findViewById(R.id.tvMessage);
+        message.setText(messageText);
+
+        TextView from = (TextView)findViewById(R.id.tvFrom);
+        from.setText("From: DemoUser");
+    }
+
+    public void bBackClick(View view) {
+        Intent main = new Intent(this, MainActivity.class);
+        startActivity(main);
+    }
+
+    public void bRespondClick(View view) {
+        Intent response = new Intent(this, MessageActivity.class);
+        startActivity(response);
     }
 }
