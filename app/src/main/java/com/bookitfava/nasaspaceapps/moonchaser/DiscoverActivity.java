26097414eba0e42f0fab3c0c4abbc0ed1e410c41
Feko <ferid.dragon@hotmail.com>
2016-04-22 22:21:05 +0200

    Discover prototype

--- app/src/main/java/com/bookitfava/nasaspaceapps/moonchaser/DiscoverActivity.java
+++ app/src/main/java/com/bookitfava/nasaspaceapps/moonchaser/DiscoverActivity.java
@@ -1,16 +1,71 @@
 package com.bookitfava.nasaspaceapps.moonchaser;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
-public class DiscoverActivity extends AppCompatActivity {
-    ImageView imageView;
+public class DiscoverActivity extends AppCompatActivity implements SensorEventListener {
+    private CustomDrawableView cdv;
+    private Bitmap bmMoon;
+    private int x;
+    private int y;
+    private SensorManager sensorManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_discover);
+
+        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        cdv = new CustomDrawableView(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            x = (int)event.values[0];
+            y = (int)event.values[1];
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onStop() {
+        sensorManager.unregisterListener(this);
+        super.onStop();
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        //izlishno
+    }
+
+    public class CustomDrawableView extends View {
+
+        public CustomDrawableView(Context context) {
+            super(context);
+            bmMoon = BitmapFactory.decodeResource(getResources(), R.drawable.themoon);
+        }
+
+        protected void onDraw(Canvas canvas) {
+            canvas.drawBitmap(bmMoon, x, y, new Paint());
+            invalidate();
+        }
 
     }
 }
