09c9268c7f9be654babb61e844c4bf0a65e34f8f
Ferid <ferid.dragon@hotmail.com>
2016-04-22 18:18:39 +0200

    MessageActivity

--- app/src/main/java/com/bookitfava/nasaspaceapps/moonchaser/MessageActivity.java
+++ app/src/main/java/com/bookitfava/nasaspaceapps/moonchaser/MessageActivity.java
@@ -1,7 +1,13 @@
 package com.bookitfava.nasaspaceapps.moonchaser;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MessageActivity extends AppCompatActivity {
 
@@ -9,5 +15,25 @@ public class MessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
+
+        ImageView logoView = (ImageView) findViewById(R.id.ivLogo);
+        Bitmap logo = BitmapFactory.decodeResource(getResources(), R.drawable.themoon);
+        logoView.setImageBitmap(logo);
+    }
+
+    public void bBackClick(View view) {
+        Intent main = new Intent(this, MainActivity.class);
+        startActivity(main);
+    }
+
+    public void bSendClick(View view) {
+        EditText message = (EditText)findViewById(R.id.etMessage);
+        String messageText = message.getText().toString();
+
+        Intent discover = new Intent(this, DiscoverActivity.class);
+        Bundle bundle = new Bundle();
+        bundle.putString("message", messageText);
+        discover.putExtras(bundle);
+        startActivity(discover);
     }
 }
