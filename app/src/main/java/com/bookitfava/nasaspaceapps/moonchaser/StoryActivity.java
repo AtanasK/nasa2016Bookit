bc4dd5d5083425f3bbeb72e9c822a96dcc6200a2
Atanas Kostovski <atanasmk16@gmail.com>
2016-04-22 16:56:40 +0200

    slika

--- app/src/main/java/com/bookitfava/nasaspaceapps/moonchaser/StoryActivity.java
+++ app/src/main/java/com/bookitfava/nasaspaceapps/moonchaser/StoryActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class StoryActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
