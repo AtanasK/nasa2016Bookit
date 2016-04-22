f795e1a1629db2828469378b91a75946bbbb41c5
Feko <ferid.dragon@hotmail.com>
2016-04-22 19:34:06 +0200

    MainActivity

--- app/src/main/java/com/bookitfava/nasaspaceapps/moonchaser/MainActivity.java
+++ app/src/main/java/com/bookitfava/nasaspaceapps/moonchaser/MainActivity.java
@@ -1,9 +1,12 @@
 package com.bookitfava.nasaspaceapps.moonchaser;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,10 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView logo = (ImageView)findViewById(R.id.ivLogo);
+        Bitmap logoImage = BitmapFactory.decodeResource(getResources(), R.drawable.themoon);
+        logo.setImageBitmap(logoImage);
     }
 
     public void gotoStory(View view) {
         Intent intent = new Intent(this, StoryActivity.class);
         startActivity(intent);
+    }
+
+    public void bShareClick(View view) {
+        Intent message = new Intent(this, MessageActivity.class);
+        startActivity(message);
     }
 }
